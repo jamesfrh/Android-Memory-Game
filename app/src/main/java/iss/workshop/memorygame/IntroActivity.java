82210c754924909e4bba25950e007d8bfa3f8fdc
Chonght <60685286+HengTat@users.noreply.github.com>
2020-07-18 13:34:47 +0800

    edited intro and added tutorial

--- app/src/main/java/iss/workshop/memorygame/IntroActivity.java
+++ app/src/main/java/iss/workshop/memorygame/IntroActivity.java
@@ -5,21 +5,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
 
-public class IntroActivity extends AppCompatActivity {
+public class IntroActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
-
-        final Intent intent= new Intent(this, MainActivity.class);
-        final Handler handler2 = new Handler();
-        handler2.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                startActivity(intent);
-            }
-        }, 1250);
+        TextView tutorial = (TextView)findViewById(R.id.Tutorialtext);
+        tutorial.setOnClickListener(this);
+        TextView skiptutorial = (TextView)findViewById(R.id.skiptutorialtext);
+        skiptutorial.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View view) {
+
+        if(view.getId()==R.id.Tutorialtext){
+            final Intent intent=new Intent(this,Tutorial.class);
+            startActivity(intent);
+        }
+
+        if(view.getId()==R.id.skiptutorialtext){
+            final Intent intent= new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
+
+    }
+
 }
---
+++ app/src/main/java/iss/workshop/memorygame/Tutorial.java
@@ -0,0 +1,41 @@
+package iss.workshop.memorygame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class Tutorial extends AppCompatActivity implements View.OnClickListener {
+
+    int clickcounter= 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutorial);
+        TextView nextbutton= (TextView) findViewById(R.id.Next);
+        nextbutton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.Next) {
+            if (clickcounter == 0) {
+                TextView pagetitle = (TextView) findViewById(R.id.pagetitle);
+                pagetitle.setText("Match Images");
+                ImageView tutorialimage = (ImageView) findViewById(R.id.Tuorialimage);
+                tutorialimage.setImageResource(R.drawable.tutorialpage2);
+                TextView nextbutton = (TextView) findViewById(R.id.Next);
+                nextbutton.setText("Done");
+                clickcounter++;
+            } else if (clickcounter == 1) {
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+            }
+
+        }
+    }
+}
